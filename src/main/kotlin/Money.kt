7ce5aed085a5f7c68a15578ccff1554f24dfec04
horyuji <jeyjeyjeey@gmail.com>
2021-11-28 19:50:51 +0900

    １０章まで

--- src/main/kotlin/Money.kt
+++ src/main/kotlin/Money.kt
@@ -2,25 +2,21 @@ package money
 
 class Dollar(amount: Int, currency: String): Money(amount, currency) {
 
-    override fun times(multiplier: Int) = dollar(this.amount * multiplier)
-
 }
 
 class Franc(amount: Int, currency: String): Money(amount, currency) {
 
-    override fun times(multiplier: Int) = franc(this.amount * multiplier)
-
 }
 
-abstract class Money(protected val amount: Int, open val currency: String) {
+open class Money(protected val amount: Int, open val currency: String) {
 
-    override fun equals(other: Any?) = (other as? Money)?.amount == this.amount && this.javaClass == other.javaClass
+    override fun equals(other: Any?) = (other as? Money)?.amount == this.amount && this.currency == other.currency
 
     companion object {
-        fun dollar(amount: Int) = Dollar(amount, "USD")
-        fun franc(amount: Int) = Franc(amount, "CHF")
+        fun dollar(amount: Int) = Money(amount, "USD")
+        fun franc(amount: Int) = Money(amount, "CHF")
     }
 
-    abstract fun times(multiplier: Int): Money
+    fun times(multiplier: Int) = Money(amount * multiplier, currency)
 
 }
